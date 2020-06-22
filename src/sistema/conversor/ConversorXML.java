ad52b4256bc33246b7bafe0351fea0cb1cc8ecbf
gustavo <gustavodreche@msn.com>
2020-06-22 12:29:57 -0300

    Criacao do conversor de XML

---
+++ src/sistema/conversor/ConversorXML.java
@@ -0,0 +1,64 @@
+package sistema.conversor;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+
+public class ConversorXML {
+	
+	private static final String TAG_LISTA = "<lista>";
+	private static final String TAG_LISTA_FECHAMENTO = "</lista>";
+	private static final String TAG_ABERTURA = "<";
+	private static final String TAG_FECHAMENTO = ">";
+	private static final String TAG_ABERTURA_FECHA_TAG = "</";
+
+	public String converte(Object objeto) {
+		try {
+			Class<?> classeObjeto = objeto.getClass();
+			StringBuffer xml = new StringBuffer();
+
+			// CASO RECEBA UMA LISTA, A TAG DO XML MUDA
+			if (objeto instanceof Collection) {
+				trataListaDeObjetoParaXml(objeto, xml);
+			} else {
+				trataObjetoParaXml(objeto, classeObjeto, xml);
+			}
+
+			return xml.toString();
+
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Erro na geração do XML!");
+
+		}
+	}
+
+	private void trataObjetoParaXml(Object objeto, Class<?> classeObjeto, StringBuffer xml) throws IllegalAccessException {
+		String nomeClasse = classeObjeto.getName();
+		xml.append(TAG_ABERTURA + nomeClasse + TAG_FECHAMENTO);
+
+		for (Field atributo : classeObjeto.getDeclaredFields()) {
+			atributo.setAccessible(true);
+
+			String nomeAtributo = atributo.getName();
+
+			Object valorAtributo = atributo.get(objeto);
+
+			xml.append(TAG_ABERTURA + nomeAtributo + TAG_FECHAMENTO);
+			xml.append(valorAtributo);
+			xml.append(TAG_ABERTURA_FECHA_TAG + nomeAtributo + TAG_FECHAMENTO);
+		}
+
+		xml.append(TAG_ABERTURA_FECHA_TAG + nomeClasse + TAG_FECHAMENTO);
+	}
+
+	private void trataListaDeObjetoParaXml(Object objeto, StringBuffer xml) {
+		Collection<?> listaDeObjeto = (Collection<?>) objeto;
+		xml.append(TAG_LISTA);
+		for (Object umObjeto : listaDeObjeto) {
+			String xmlDeUmObjeto = converte(umObjeto);
+			xml.append(xmlDeUmObjeto);
+		}
+		xml.append(TAG_LISTA_FECHAMENTO);
+	}
+
+}
--- src/sistema/main/Main.java
+++ src/sistema/main/Main.java
@@ -1,8 +1,22 @@
 package sistema.main;
 
+import java.util.ArrayList;
+
+import sistema.conversor.ConversorXML;
+import sistema.modelo.Produto;
+
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		Produto p1 = new Produto(123456, "COCA", 3.50);
+		Produto p2 = new Produto(99999, "PAO DE QUEIJO", 1.20);
+		ArrayList<Produto> listaProduto = new ArrayList<Produto>();
+		listaProduto.add(p1);
+		listaProduto.add(p2);
+		
+		String converte = new ConversorXML().converte(listaProduto);
+		System.out.println(converte);
 		
 	}
 
